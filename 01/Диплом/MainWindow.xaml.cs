69172576946dc7219f287f6e8293d902ee434a71
MirnII <49513103+MirnII@users.noreply.github.com>
2019-04-12 16:32:52 +0900

    v01

---
+++ 01/Диплом/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Диплом
+{
+    
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string L { get; set; }
+        public string P { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Login_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Enter_Click(object sender, RoutedEventArgs e)
+        {
+            Sql s = new Sql();
+            s.Login = Login.Text;
+            s.Password = Password.Password;
+            int status_cod = s.Authorization();
+            string _Name = s.Name;
+            string _Roly = s.Roly;
+            Sotr so = new Sotr();
+            so.Hi.Text = so.Hi.Text + _Name;
+            so.Show();
+            Hide();
+        }
+
+        private void Reister_Click(object sender, RoutedEventArgs e)
+        {
+            Registration r = new Registration(this);
+            r.Show();
+            
+            
+            
+        }
+    }
+}
---
+++ 01/Диплом/Sql.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace Диплом
+{
+    class Sql
+    {
+        string Sql_ = ConfigurationManager.ConnectionStrings["SQL"].ConnectionString;
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Roly { get; set; }
+        
+        int status_cod = 0;
+        string answer;
+        
+        public int Authorization ()
+        {
+            SqlConnection connect = new SqlConnection(Sql_);
+            connect.Open();
+            SqlCommand command = new SqlCommand("Select * from Users", connect);
+            SqlDataReader reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+                if(Login == reader["Логин"].ToString() && Password == reader["Пароль"].ToString())
+                {
+                    Name = reader["Имя"].ToString();
+                    Roly = reader["Роль"].ToString();
+                    status_cod = 1;
+                    reader.Close();
+                    break;
+                }
+                else
+                {
+                    status_cod = 1111;
+                }
+            }
+            return status_cod;
+        }
+        
+        public string Register(Registration r)
+        {
+            Login = r.Login.Text;
+            Password = r.Password.Password;
+            Name = r.Name.Text;
+            Roly = "Сотрудник";
+            SqlConnection connect = new SqlConnection(Sql_);
+            connect.Open();
+            SqlCommand command = new SqlCommand("Insert into Users(Логин, Пароль, Имя, Роль) values('" + Login + "', '" + Password + "', '" + Name + "', '" + Roly + "')", connect);
+            int number = command.ExecuteNonQuery();
+            if(number == 1)
+            {
+                answer =  "succesfull";
+                
+            }
+            else
+            {
+                answer = "error";
+            }
+            return answer;
+            
+        }
+
+        public void Register(string login, string password)
+        {
+            Login = login;
+            Password = password;   
+            
+        }
+    }
+}
